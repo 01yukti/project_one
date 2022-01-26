6bfa77cfbbc5fe817ae718d10018ec9cf99a3063
Yukti Agrawal <yukti@Yuktis-MacBook-Pro.local>
2022-01-26 12:50:39 +0000

    remove ssh from security group

--- level1/alb.tf
+++ level1/alb.tf
@@ -4,8 +4,6 @@ resource "aws_lb" "alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  #count              = length(local.public_cidr)
-  #subnets            = aws_subnet.public.*.id
   subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   tags = {
@@ -21,13 +19,6 @@ resource "aws_security_group" "alb_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
