5f93b11192e8366ea6d771219539ecc0f0eb0739
Yukti Agrawal <yukti@Yuktis-MacBook-Pro.local>
2022-01-26 18:57:44 +0000

    update listener and target groups for LB

--- level1/alb.tf
+++ level1/alb.tf
@@ -11,6 +11,49 @@ resource "aws_lb" "alb" {
   }
 }
 
+#create target groups
+resource "aws_alb_target_group" "albtg" {
+  name     = "albtg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.example.id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+
+  tags = {
+    Name = "target_group"
+  }
+}
+
+#configure target group attachment
+resource "aws_alb_target_group_attachment" "tg_attach" {
+  count            = length(local.private_cidr)
+  target_group_arn = aws_alb_target_group.albtg.arn
+  target_id        = aws_instance.first-ec2[count.index].id
+  port             = 80
+}
+
+#configure listener
+resource "aws_alb_listener" "frontend" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.albtg.arn
+  }
+
+  tags = {
+    Name = "listener-frontend"
+  }
+}
+
 #configure security group
 resource "aws_security_group" "alb_sg" {
   name   = "alb_sg"
