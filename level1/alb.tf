3ea079869c8a37fa6e46d2a5625db8cb71d0fcbc
Yukti Agrawal <yukti@Yuktis-MacBook-Pro.local>
2022-01-26 00:01:22 +0000

    add load balancer

---
+++ level1/alb.tf
@@ -0,0 +1,46 @@
+#Configure application load balancer
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  #count              = length(local.public_cidr)
+  #subnets            = aws_subnet.public.*.id
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  tags = {
+    Name = "custom-alb"
+  }
+}
+
+#configure security group
+resource "aws_security_group" "alb_sg" {
+  name   = "alb_sg"
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+  }
+}
